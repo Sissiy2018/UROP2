4eed8b13def7d7c502c78b966afbc4bff3442b8f
Your Name <you@example.com>
2023-07-06 18:15:14 +0100

    packages

--- GBM/test.py
+++ GBM/test.py
@@ -1,6 +1,8 @@
 from pandas import read_csv
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
+#from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
+import scikeras
+from scikeras.wrappers import KerasRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 import h5py
@@ -12,10 +14,28 @@ from tensorflow.keras import metrics
 import numpy as np
 from sklearn.metrics import r2_score
 import tensorflow.keras
-import elfi
+#import elfi
 import pygtc
 import pickle
 from tensorflow.keras.layers import Dropout, Dense, Input
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
 import tensorflow as tf
+
+print(tf.__version__)
+
+import numpy as np
+import emcee
+import pygtc
+import pickle
+import matplotlib.pyplot as plt
+import time
+import tensorflow
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+import os
+import sys
+import shutil
+import random
+from glob import glob
+import time
